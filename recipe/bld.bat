7172e07815eb7977573413a5b18666afd881ced4
Isuru Fernando <isuruf@gmail.com>
2021-09-21 14:12:02 -0700

    Set MS_WIN64 to remove patch in distutils

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,7 +15,11 @@ dlltool.exe -d python%CONDA_PY%.def -l %PREFIX%\libs\libpython%CONDA_PY%.dll.a
 if errorlevel 1 exit 1
 if not exist %PREFIX%\Lib\distutils\ mkdir %PREFIX%\Lib\distutils\
 if errorlevel 1 exit 1
-echo [build]           > %PREFIX%\Lib\distutils\distutils.cfg
+echo [build_ext]           > %PREFIX%\Lib\distutils\distutils.cfg
 if errorlevel 1 exit 1
-echo compiler=mingw32 >> %PREFIX%\Lib\distutils\distutils.cfg
+echo define=MS_WIN64      >> %PREFIX%\Lib\distutils\distutils.cfg
 if errorlevel 1 exit 1
+echo compiler=mingw32     >> %PREFIX%\Lib\distutils\distutils.cfg
+if errorlevel 1 exit 1
+
+copy %PREFIX%\Lib\distutils\distutils.cfg %PREFIX%\Lib\site-packages\setuptools\distutils\

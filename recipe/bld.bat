ceee5f7e33c32c4f23b8f6b0e66c45567c497443
Isuru Fernando <isuruf@gmail.com>
2022-04-27 18:17:28 -0500

    fix syntax

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ ar cru %PREFIX%\libs\libvcruntime140.dll.a
 if %python_impl% == "pypy" (
   gendef.exe %PREFIX%\pypy3-c.dll - > python%CONDA_PY%.def
   if errorlevel 1 exit 1
-else (
+) else (
   gendef.exe %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
   if errorlevel 1 exit 1
 )

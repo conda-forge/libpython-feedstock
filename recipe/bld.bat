849fb270972b8da390a6182bfd23ff9a641e1043
Isuru Fernando <isuruf@gmail.com>
2022-04-27 16:19:51 -0500

    fix for new setuptools

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,9 @@
 :: Create an empty library for msvcrt??? since CygwinCCompiler seems to
 :: think that linking to that is a good idea (it is not).
-if "%CONDA_PY%" == "27" (
-  ar cru %PREFIX%\libs\libmsvcr90.dll.a
-) else (
-  if "%CONDA_PY%" == "34" (
-    ar cru %PREFIX%\libs\libmsvcr120.dll.a
-  ) else (
-    ar cru %PREFIX%\libs\libmsvcr140.dll.a
-  )
-)
+ar cru %PREFIX%\libs\libmsvcr140.dll.a
+ar cru %PREFIX%\libs\libucrt.dll.a
+ar cru %PREFIX%\libs\libvcruntime140.dll.a
+
 gendef.exe %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
 if errorlevel 1 exit 1
 dlltool.exe -d python%CONDA_PY%.def -l %PREFIX%\libs\libpython%CONDA_PY%.dll.a

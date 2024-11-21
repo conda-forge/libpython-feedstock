0ef075ebb538b4adc36e217aabfc42b940d8a43a
Isuru Fernando <isuruf@gmail.com>
2024-11-21 11:27:29 -0800

    remove import libraries and keep only distutils bits

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,3 @@
-:: Create an empty library for msvcrt??? since CygwinCCompiler seems to
-:: think that linking to that is a good idea (it is not).
-ar cru %PREFIX%\libs\libmsvcr140.dll.a
-ar cru %PREFIX%\libs\libucrt.dll.a
-ar cru %PREFIX%\libs\libvcruntime140.dll.a
-
-if "%python_impl%" == "pypy" (
-  if "%PY_VER%" == "3.8" (
-    gendef.exe %PREFIX%\libpypy3-c.dll - > python%CONDA_PY%.def
-  ) else (
-    gendef.exe %PREFIX%\libpypy%PY_VER%-c.dll - > python%CONDA_PY%.def
-  )
-  if errorlevel 1 exit 1
-) else (
-  gendef.exe %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
-  if errorlevel 1 exit 1
-)
-dlltool.exe -d python%CONDA_PY%.def -l %PREFIX%\libs\libpython%CONDA_PY%.dll.a
-if errorlevel 1 exit 1
 if not exist %PREFIX%\Lib\distutils\ mkdir %PREFIX%\Lib\distutils\
 if errorlevel 1 exit 1
 echo [build_ext]           > %PREFIX%\Lib\distutils\distutils.cfg

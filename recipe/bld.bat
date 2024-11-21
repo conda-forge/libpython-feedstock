ff149f15dd1e66ba9c7b940384359a3cd5d943f9
Isuru Fernando <isuruf@gmail.com>
2024-11-21 12:13:43 -0800

    debug 2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,7 @@
 echo "setting distutils cfg"
 if not exist %PREFIX%\Lib\distutils\ mkdir %PREFIX%\Lib\distutils\
 if errorlevel 1 exit 1
+dir %PREFIX%\Lib\distutils\
 echo [build_ext]           > %PREFIX%\Lib\distutils\distutils.cfg
 if errorlevel 1 exit 1
 echo define=MS_WIN64      >> %PREFIX%\Lib\distutils\distutils.cfg
@@ -8,4 +9,6 @@ if errorlevel 1 exit 1
 echo compiler=mingw32     >> %PREFIX%\Lib\distutils\distutils.cfg
 if errorlevel 1 exit 1
 
+echo "setting setuptools cfg"
+dir %PREFIX%\Lib\site-packages\setuptools\_distutils\
 copy %PREFIX%\Lib\distutils\distutils.cfg %PREFIX%\Lib\site-packages\setuptools\_distutils\
